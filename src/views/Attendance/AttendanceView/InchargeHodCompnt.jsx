be680fcba0dea4a87010eaeb7bc6928cd7b6f94c
unknown <Administrator@MEDICITY.CO.IN>
2024-10-30 10:32:25 +0530

    error correction

--- src/views/Attendance/AttendanceView/InchargeHodCompnt.jsx
+++ src/views/Attendance/AttendanceView/InchargeHodCompnt.jsx
@@ -107,18 +107,22 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                 }
                             }),
                             totalDays: dateRange?.length,
-                            totalP: empArray?.filter(el => el.lvereq_desc === "P").length ?? 0,
+                            // totalP: empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC").length ?? 0,
+                            // totalP: (empArray?.filter(el => el.lvereq_desc === "DP").length ?? 0) * 2 + (empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC" || el.lvereq_desc === "WP").length ?? 0),
+                            totalP: empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC").length ?? 0,
                             totalWOFF: empArray?.filter(el => el.lvereq_desc === "WOFF").length ?? 0,
                             totalNOFF: empArray?.filter(el => el.lvereq_desc === "NOFF").length ?? 0,
-                            totalLC: empArray?.filter(el => el.duty_desc === "LC").length ?? 0,
-                            totalHD: empArray?.filter(el => el.lvereq_desc === "HD").length ?? 0,
+                            totalLC: empArray?.filter(el => el.lvereq_desc === "LC").length ?? 0,
+                            totalHD: empArray?.filter(el => el.lvereq_desc === "CHD" || el.lvereq_desc === "HD" || el.lvereq_desc === "EGHD").length ?? 0,
                             totalA: empArray?.filter(el => el.lvereq_desc === "A").length ?? 0,
                             totalLV: empArray?.filter(el => el.lvereq_desc === "COFF" || el.lvereq_desc === "CL" || el.lvereq_desc === "EL" || el.lvereq_desc === "SL").length ?? 0,
-                            totalHDL: (empArray?.filter(el => el.lvereq_desc === "HDL").length ?? 0) * 1,
+                            totalHDL: (empArray?.filter(el => el.lvereq_desc === "HCL").length ?? 0) * 1,
                             totaESI: empArray?.filter(el => el.lvereq_desc === "ESI").length ?? 0,
                             totaLWP: empArray?.filter(el => el.lvereq_desc === "LWP").length ?? 0,
                             totaH: empArray?.filter(el => el.lvereq_desc === "H").length ?? 0,
                             totaHP: grossSalary <= salary_above ? (empArray?.filter(el => el.lvereq_desc === "HP").length ?? 0) * 2 : (empArray?.filter(el => el.lvereq_desc === "H").length ?? 0),
+                            totaWP: empArray?.filter(el => el.lvereq_desc === "WP").length ?? 0,
+                            totaDP: empArray?.filter(el => el.lvereq_desc === "DP").length ?? 0,
                         }
                     })
                     settableArray(resultss)
@@ -183,11 +187,12 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                     }),
                     totalDays: dateRange?.length,
                     // totalP: empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC").length ?? 0,
-                    totalP: (empArray?.filter(el => el.lvereq_desc === "DP").length ?? 0) * 2 + (empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC").length ?? 0),
+                    // totalP: (empArray?.filter(el => el.lvereq_desc === "DP").length ?? 0) * 2 + (empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC" || el.lvereq_desc === "WP").length ?? 0),
+                    totalP: empArray?.filter(el => el.lvereq_desc === "P" || el.lvereq_desc === "OHP" || el.lvereq_desc === "ODP" || el.lvereq_desc === "LC").length ?? 0,
                     totalWOFF: empArray?.filter(el => el.lvereq_desc === "WOFF").length ?? 0,
                     totalNOFF: empArray?.filter(el => el.lvereq_desc === "NOFF").length ?? 0,
                     totalLC: empArray?.filter(el => el.lvereq_desc === "LC").length ?? 0,
-                    totalHD: empArray?.filter(el => el.lvereq_desc === "CHD" || el.lvereq_desc === "HD").length ?? 0,
+                    totalHD: empArray?.filter(el => el.lvereq_desc === "CHD" || el.lvereq_desc === "HD" || el.lvereq_desc === "EGHD").length ?? 0,
                     totalA: empArray?.filter(el => el.lvereq_desc === "A").length ?? 0,
                     totalLV: empArray?.filter(el => el.lvereq_desc === "COFF" || el.lvereq_desc === "CL" || el.lvereq_desc === "EL" || el.lvereq_desc === "SL").length ?? 0,
                     totalHDL: (empArray?.filter(el => el.lvereq_desc === "HCL").length ?? 0) * 1,
@@ -195,6 +200,8 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                     totaLWP: empArray?.filter(el => el.lvereq_desc === "LWP").length ?? 0,
                     totaH: empArray?.filter(el => el.lvereq_desc === "H").length ?? 0,
                     totaHP: grossSalary <= salary_above ? (empArray?.filter(el => el.lvereq_desc === "HP").length ?? 0) * 2 : (empArray?.filter(el => el.lvereq_desc === "H").length ?? 0),
+                    totaWP: empArray?.filter(el => el.lvereq_desc === "WP").length ?? 0,
+                    totaDP: empArray?.filter(el => el.lvereq_desc === "DP").length ?? 0,
                 }
             })
             settableArray(resultss)
@@ -234,6 +241,8 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
         { lvename: 'HP', color: 'success', desc: "Holiday Present" },
         { lvename: 'ML', color: 'danger', desc: "Maternity Leave" },
         { lvename: 'LC', color: 'danger', desc: "Late Coming" },
+        { lvename: 'DP', color: 'success', desc: "Double Present" },
+        { lvename: 'WP', color: 'success', desc: "Week Off Present" },
     ]
 
     return (
@@ -359,6 +368,8 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                 <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
                                                 <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
                                                 <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
+                                                <th style={{ width: 65, backgroundColor: '#f4f6f8' }} ></th>
+                                                <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
                                             </tr>
                                             <tr>
                                                 <th style={{ zIndex: 5, backgroundColor: '#b1b9c0' }}> Days </th>
@@ -376,6 +387,8 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > H</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > HP</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > LV</th>
+                                                <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > DP</th>
+                                                <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > WP</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > A</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > ESI</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > Calc. Days</th>
@@ -423,6 +436,8 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
                                                     </tr>
                                                     <tr>
                                                         {row.punchMaster.map((val, ind) => (
@@ -450,9 +465,11 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaH}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaHP}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalLV + row.totalHDL}</td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaDP}</td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaWP}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaLWP + row.totalA}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaESI}</td>
-                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalP + row.totalWOFF + row.totalNOFF + row.totalLV + (row.totalHD * 0.5) + row.totaHP}</td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalP + row.totalWOFF + row.totalNOFF + row.totalLV + (row.totalHD * 0.5) + row.totaHP + row.totaDP + row.totaWP}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalDays}</td>
                                                     </tr>
                                                 </Fragment>
@@ -587,6 +604,9 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                 <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
                                                 <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
                                                 <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
+                                                <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
+                                                <th style={{ width: 65, backgroundColor: '#f4f6f8' }} ></th>
+                                                <th style={{ width: 60, backgroundColor: '#f4f6f8' }} ></th>
                                             </tr>
                                             <tr>
                                                 <th style={{ zIndex: 5, backgroundColor: '#b1b9c0' }}> Days </th>
@@ -604,8 +624,11 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > H</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > HP</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > LV</th>
+                                                <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > DP</th>
+                                                <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > WP</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > A</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > ESI</th>
+                                                <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > Calc. Days</th>
                                                 <th style={{ textAlign: 'center', backgroundColor: '#f4f6f8', color: '#635bff' }} > Days</th>
                                             </tr>
                                         </thead>
@@ -649,6 +672,9 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: 'lightgray' }}></td>
                                                     </tr>
                                                     <tr>
                                                         {row.punchMaster.map((val, ind) => (
@@ -676,8 +702,11 @@ const InchargeHodCompnt = ({ em_id, em_no }) => {
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaH}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaHP}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalLV + row.totalHDL}</td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaDP}</td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaWP}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaLWP + row.totalA}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totaESI}</td>
+                                                        <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalP + row.totalWOFF + row.totalNOFF + row.totalLV + (row.totalHD * 0.5) + row.totaHP + row.totaDP + row.totaWP}</td>
                                                         <td style={{ textAlign: 'center', height: 10, color: '#344767', fontWeight: 900, backgroundColor: isOdd(index) ? '#f4f6f8' : '#f4f6f8' }}>{row.totalDays}</td>
                                                     </tr>
                                                 </Fragment>
